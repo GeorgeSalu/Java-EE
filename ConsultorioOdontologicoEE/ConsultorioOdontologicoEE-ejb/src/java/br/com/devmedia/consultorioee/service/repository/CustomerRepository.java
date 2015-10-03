0b942b07f9d95dc742598d5ac4ea61a85289cbed
George salu <george.salu10@gmail.com>
2015-10-03 16:27:53 -0300

    finalização do repository de Customer

--- ConsultorioOdontologicoEE/ConsultorioOdontologicoEE-ejb/src/java/br/com/devmedia/consultorioee/service/repository/CustomerRepository.java
+++ ConsultorioOdontologicoEE/ConsultorioOdontologicoEE-ejb/src/java/br/com/devmedia/consultorioee/service/repository/CustomerRepository.java
@@ -6,6 +6,9 @@
 package br.com.devmedia.consultorioee.service.repository;
 
 import br.com.devmedia.consultorioee.entities.Customer;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 import javax.persistence.EntityManager;
 
 /**
@@ -34,6 +37,28 @@ public class CustomerRepository extends BasicRepository{
     
     public Customer getCustomer(Customer customer) {
         return getEntity(Customer.class, customer);
+    }
+    
+    public List<Customer> getCustomerByName(String name) {
+        return getPureList(Customer.class,"select cus from Customer cus where cus.cusName like ?1",name+"%");
+    }
+    
+    public List<Customer> getCustomersToCall(int month,int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, (month-1));
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        Date dataInicial = cal.getTime();
+        
+        cal.add(Calendar.MONTH, 1);
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        Date dataFinal = cal.getTime();
+        
+        return getPureList(Customer.class,"select orc.orcCustomer from Oracamento orc where orc.orcDate >= ?1 and orc.orcDate <= ?2",dataInicial,dataFinal);
+    }
+
+    public List<Customer> getCustomersComPagamentoEmAberto(int idOfCustomer) {
+        return getPureList(Customer.class,"select par.parOrcamento.orcCustomer from Parcela par where par.parPago = ?1",idOfCustomer,Boolean.FALSE);
     }
     
 }
