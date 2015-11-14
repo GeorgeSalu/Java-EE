fdf8acbc115e0e86dc900f17a0d24c4287c9c69b
George salu <george.salu10@gmail.com>
2015-11-14 12:18:34 -0200

    Alterar Item do Orçamento

--- ConsultorioOdontologicoEE/ConsutorioOdontologicoEE-war/src/java/br/com/devmedia/consultorioee/control/OrcamentoControl.java
+++ ConsultorioOdontologicoEE/ConsutorioOdontologicoEE-war/src/java/br/com/devmedia/consultorioee/control/OrcamentoControl.java
@@ -38,7 +38,7 @@ public class OrcamentoControl extends BasicControl implements java.io.Serializab
     private Customer selectedCustomer;
     private Orcamento selectedOrcamento;
     private Orcamentoitem selectedOrcamentoItem;
-    
+
     private List<Orcamento> orcamentos;
 
     public OrcamentoService getOrcamentoService() {
@@ -52,7 +52,7 @@ public class OrcamentoControl extends BasicControl implements java.io.Serializab
     public void setSelectedCustomer(Customer selectedCustomer) {
         this.selectedCustomer = selectedCustomer;
     }
-    
+
     public void setOrcamentoService(OrcamentoService orcamentoService) {
         this.orcamentoService = orcamentoService;
     }
@@ -72,7 +72,7 @@ public class OrcamentoControl extends BasicControl implements java.io.Serializab
     public void setOrcamentos(List<Orcamento> orcamentos) {
         this.orcamentos = orcamentos;
     }
-    
+
     public String doStartAtenderOCliente(Customer customer) {
         setSelectedCustomer(customer);
         anamneseControl.setSelectedCustomer(selectedCustomer);
@@ -84,31 +84,31 @@ public class OrcamentoControl extends BasicControl implements java.io.Serializab
     public String getItensOfOrcamento(Orcamento orc) {
         StringBuilder sb = new StringBuilder();
         for (Orcamentoitem item : orc.getOrcamentoitemList()) {
-            if (sb.length() >0) {
-               sb.append(",");
+            if (sb.length() > 0) {
+                sb.append(",");
             }
             sb.append(item.getOriService().getSrvName());
         }
         return sb.toString();
     }
+
     private void cleanCache() {
         setSelectedOrcamento(new Orcamento());
         getSelectedOrcamento().setOrcCustomer(getSelectedCustomer());
         setOrcamentos(orcamentoService.getOrcamentos(getSelectedCustomer().getCusId()));
     }
-    
+
     public String doStartOrcamentoComUmaAnamnese(Anaminese ans) {
         cleanCache();
         getSelectedOrcamento().setOrcAnaminese(ans);
         ans.setAnsOrcamento(selectedOrcamento);
         return "/restrito/addOrcamento.faces";
     }
-    
+
     public List<Users> getDentistas() {
         return userService.getDentistUsers();
     }
-    
-    
+
     public List<PaymentType> getPaymentTypes() {
         return Arrays.asList(PaymentType.values());
     }
@@ -120,23 +120,23 @@ public class OrcamentoControl extends BasicControl implements java.io.Serializab
     public void setSelectedOrcamentoItem(Orcamentoitem selectedOrcamentoItem) {
         this.selectedOrcamentoItem = selectedOrcamentoItem;
     }
-    
+
     public List<Service> getServices() {
         return serviceService.getServices();
     }
-    
+
     public String doStartAddItemAoOrcamento() {
         selectedOrcamentoItem = new Orcamentoitem();
         selectedOrcamentoItem.setOriOrcamento(selectedOrcamento);
         return "/restrito/addOrcamentoItem.faces";
     }
-    
+
     public String doFinishExcluirItem() {
         selectedOrcamento.getOrcamentoitemList().remove(selectedOrcamentoItem);
         recalcularSaldoOrcamento();
         return "/restrito/addOrcamento.faces";
     }
-    
+
     public String doFinishAddOrcamentoItem() {
         selectedOrcamentoItem.setOriCost(selectedOrcamentoItem.getTotalItemParcial());
         selectedOrcamento.getOrcamentoitemList().add(selectedOrcamentoItem);
@@ -150,5 +150,18 @@ public class OrcamentoControl extends BasicControl implements java.io.Serializab
             total = total.add(item.getOriCost());
         }
         selectedOrcamento.setOrcTotal(total);
+    }
+
+    public String doStartAlterarItemOrcamento() {
+        return "/restrito/editOrcamentoItem.faces";
+    }
+
+    public String doFinishEditOrcamentoItem() {
+        if (selectedOrcamentoItem.getOriId() != null) {
+            orcamentoService.setItem(selectedOrcamentoItem);
+        }
+        selectedOrcamentoItem.setOriCost(selectedOrcamentoItem.getTotalItemParcial());
+        recalcularSaldoOrcamento();
+        return "/restrito/addOrcamento.faces";
     }
 }
