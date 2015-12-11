df3a6eaa8282070a11eba6f09c804cd61f58f67f
George salu <george.salu10@gmail.com>
2015-12-10 22:52:06 -0200

    Recuperando as imagens via AJAX

---
+++ ConsultorioOdontologicoEE/ConsutorioOdontologicoEE-war/src/java/br/com/devmedia/consultorioee/servlet/ImageServlet.java
@@ -0,0 +1,81 @@
+package br.com.devmedia.consultorioee.servlet;
+
+import br.com.devmedia.consultorioee.entities.Imagem;
+import br.com.devmedia.consultorioee.service.ImageService;
+import java.io.IOException;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author George salu
+ */
+@WebServlet(name = "ImageServlet", urlPatterns = {"/ImageServlet"})
+public class ImageServlet extends HttpServlet {
+    
+    @EJB
+    private ImageService imageService;
+    
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Integer idImagem = Integer.parseInt(request.getParameter("id"));
+        Imagem img = imageService.getImagem(idImagem);
+        response.setContentType("image/jpeg;charset=UTF-8");
+        response.getOutputStream().write(img.getImgImagem());
+        response.flushBuffer();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
